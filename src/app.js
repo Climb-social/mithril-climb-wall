3a9579c21edb38831cea26ec1151e3b4f6ecbd3f
Michele Memoli <michele@100shapes.com>
2015-09-11 16:31:34 +0100

    Successfully rendering climb.social feed

--- src/app.js
+++ src/app.js
@@ -1,1 +1,37 @@
-window.console.log('Ready');
+var climb = {};
+
+climb.Collection = {
+    feed() {
+        "use strict";
+        const COLLECTION_ID = '55c1e00845284e26681c4929';
+
+        return m.request({
+            dataType: "jsonp",
+            url: `http://app.climb.social/api/v1/collections/${COLLECTION_ID}`
+        });
+    }
+};
+
+climb.Wall = {
+
+    controller: function () {
+        var feed = climb.Collection.feed();
+        return {
+            feed
+        }
+
+    },
+
+    view: function (ctrl) {
+        "use strict";
+        return m('div', [
+            ctrl.feed().map(function (item) {
+                return m('div', item.message);
+            })
+        ]);
+    }
+};
+
+
+//initialize
+m.mount(document.getElementById("climb-wall"), climb.Wall);
--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,9 @@
 
 var getConfig = require('hjs-webpack');
 
-
 module.exports = getConfig({
     in: 'src/app.js',
     out: 'dist',
+    html: false,
     clearBeforeBuild: true
 });
