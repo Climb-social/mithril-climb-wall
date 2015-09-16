995ca586fce482e65e580e4da83dbf3a00ba640a
Michele Memoli <michele@100shapes.com>
2015-09-16 23:29:10 +0100

    Moved mithril to externals

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,22 @@
 
 var getConfig = require('hjs-webpack');
 
-module.exports = getConfig({
+var config = getConfig({
     in: 'src/app.js',
     out: 'dist',
     clearBeforeBuild: true,
     html: function (context) {
         return {
             'index.html': context.defaultTemplate({
-                html: `<div class="climb-wall" data-collection-id="55a7d29945284ef60c0ce772"></div>`
+                html: `<script src="https://cdnjs.cloudflare.com/ajax/libs/mithril/0.2.0/mithril.min.js"></script>
+                       <div class="climb-wall" data-collection-id="55a7d29945284ef60c0ce772"></div>`
             })
         }
     }
 });
+
+config.externals = {
+    mithril: 'm'
+};
+
+module.exports = config;
